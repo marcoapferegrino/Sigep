4f1771e7fec5ff4343162e501b35cc61f1e00897
fuce <ferefuc@gmail.com>
2015-06-15 22:43:48 -0500

    Esquemas de modelos
    Se crearon los espiemos de los modelos faltan las relaciones

---
+++ app/Entities/ArchivoAlumno.php
@@ -0,0 +1,9 @@
+<?php namespace PosgradoService\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ArchivoAlumno extends Model {
+
+	//
+
+}
---
+++ app/Entities/Periodo.php
@@ -0,0 +1,9 @@
+<?php namespace PosgradoService\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Periodo extends Model {
+
+	//
+
+}
